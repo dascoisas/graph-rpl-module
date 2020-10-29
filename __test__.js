d4233f2f93490988cd9c61b9a01180a6520c4e3a
Drauzio R. Oppenheimer <drauzio.oppenheimer@gmail.com>
2020-10-29 14:31:22 -0300

    update files to match with node modules import

---
+++ __test__.js
@@ -0,0 +1,21 @@
+const Graph = require('./index')
+
+graph = new Graph("cccc::2")
+
+const main = () => {
+	graph.addCoordinator({ address: "a" })
+	graph.addVertex({ address: "b", parents: ["a"] })
+	graph.addVertex({ address: "c", parents: ["a"] })
+	graph.addVertex({ address: "d", parents: ["b"] })
+	graph.addVertex({ address: "e", parents: ["b"] })
+	graph.addVertex({ address: "f", parents: ["c"] })
+	graph.addVertex({ address: "g", parents: ["c"] })
+	graph.addVertex({ address: "h", parents: ["c"] })
+	graph.addVertex({ address: "i", parents: ["d"] })
+	graph.addVertex({ address: "j", parents: ["d"] })
+	graph.addVertex({ address: "k", parents: ["d"] })
+	graph.addVertex({ address: "l", parents: ["f"] })
+	console.log(graph.getPath("d", 0))
+}
+
+main()
